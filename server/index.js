ec02ecddf2db9d76645ad46d46911b6f039884cb
laurarengifo09 <rengifolaura09@gmail.com>
2022-10-19 11:05:57 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,4 +31,4 @@ const encontrarMejorSiesta = (agenda) => {
 }
 
 console.log(resolverCaso("16:59 18:23 Otras,45:35 46:34 Otras,28:22 28:54 Reunión,41:52 42:10 Almuerzo,21:53 22:50 Reunión,33:42 34:34 Reunión,08:30 09:24 Otras--44:56 45:23 Reunión,08:47 10:04 Reunión,24:45 24:57 Reunión,35:03 35:34 PrepaClase,28:34 29:31 Reunión,20:48 21:06 Almuerzo"))
-console.log('hola')
+console.log('holaaaa')
